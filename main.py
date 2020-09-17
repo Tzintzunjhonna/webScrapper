ff55e97f560a0294737773338cd53d613e83f6f5
Jhonathan Tzintzun <jjctjhonathan@gmail.com>
2020-09-17 18:37:11 -0500

    se implementa la extraccion de links

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+import news_page_objects as news
 from common import config
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,11 @@ def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
     logging.info(f'Iniciando scrape para {host}')
+
+    homepage= news.HomePage(news_site_uid, host)
+
+    for link in homepage.article_links:
+        print(link)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -22,3 +28,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     _news_scraper(args.news_site)
+
+    
