80ebb2d960f043729ea24c76ff41aa47c72c83cc
Jhonathan Tzintzun <jjctjhonathan@gmail.com>
2020-09-17 18:14:23 -0500

    queda la parte inicial del proyecto

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+from common import config
+
+logger = logging.getLogger(__name__)
+
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
+
+    logging.info(f'Iniciando scrape para {host}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    news_site_choices = list(config()['news_sites'].keys())
+    parser.add_argument('news_site',
+                        help = 'El nuevo sitio que tu quieres scrapear',
+                        type = str,
+                        choices = news_site_choices)
+
+    args = parser.parse_args()
+    _news_scraper(args.news_site)
