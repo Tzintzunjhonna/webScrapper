1f0b1c1e9f7259613e1919682cc94237190c9e1a
Jhonathan Tzintzun <jjctjhonathan@gmail.com>
2020-09-21 15:08:02 -0500

    Se implementa la extraccion de datos de los articulos

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 import logging
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+import re
+
+is_well_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,6 +23,41 @@ def _news_scraper(news_site_uid):
 
     for link in homepage.article_links:
         print(link)
+
+    articles = []
+    for link in homepage.article_links:
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched')
+            articles.append(article)
+            print(article.title)
+
+    print(len(articles))
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info(f'Start fetching article at {link}')
+
+    article = None
+
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. there is not body')
+        return None
+
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return f'{host}{link}'
+    else:
+        return f'{host}/{link}'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
